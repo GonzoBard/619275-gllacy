f5e21283a9ebd85a61d41c1a9fb1f55b9806e6cb
gonzo.bard <gonzo.bard@gmail.com>
2018-03-19 22:49:44 +0300

    some fixes

--- js/script.js
+++ js/script.js
@@ -60,7 +60,7 @@ function FeedBackModal() {
   //http://emailregex.com/
   var emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  this.open = function () {
+  this.open = function (event) {
     event.preventDefault(); //отменить стандартное действие
     modal.classList.remove(closeClassName);
     modal.classList.add(openClassName);
